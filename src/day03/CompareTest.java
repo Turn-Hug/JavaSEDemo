21575763856f2e256d9e4f2ce27849c8b2fe283b
Hequan <1123847545@qq.com>
2022-05-07 11:15:34 +0800

    2022/5/7

--- src/day03/CompareTest.java
+++ src/day03/CompareTest.java
@@ -2,7 +2,31 @@ package day03;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class CompareTest {
+    /*
+        String实现了Comparable接口，重写了CompareTo(obj)方法
+     */
     @Test
+    public void test1(){
+        String[] arr = new String[]{"AA","CC","ZZ","MM","DD","JJ"};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    @Test
+    public void test2(){
+        Goods[] arr = new Goods[4];
+        arr[0] = new Goods("lenvonMouse",34);
+        arr[1] = new Goods("dellMouse",12);
+        arr[2] = new Goods("xiaomiMouse",12);
+        arr[3] = new Goods("huaweiMouse",65);
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
 
 }
