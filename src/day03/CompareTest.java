77a835cec31c1b5e75729f656806cd41ba0a9522
Hequan <1123847545@qq.com>
2022-05-07 22:57:13 +0800

    2022/5/7

--- src/day03/CompareTest.java
+++ src/day03/CompareTest.java
@@ -3,6 +3,7 @@ package day03;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class CompareTest {
     /*
@@ -16,6 +17,7 @@ public class CompareTest {
 
     }
 
+    //Comparable接口的使用  自然排序
     @Test
     public void test2(){
         Goods[] arr = new Goods[4];
@@ -29,4 +31,30 @@ public class CompareTest {
         System.out.println(Arrays.toString(arr));
     }
 
+    //Comparator接口使用，定制排序
+    /*
+        当前元素的类型没有实现Comparable接口而又不方便修改代码，
+        或者实现了Comparable接口的，但排序规则不适合当前的操作，
+        则使用Comparator的对象来排序
+     */
+    @Test
+    public void test3(){
+
+        String[] arr = new String[]{"AA","CC","ZZ","MM","DD","JJ"};
+
+        Arrays.sort(arr, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof String && o2 instanceof String){
+                    String str1 = (String) o1;
+                    String str2 = (String) o2;
+                    return -str1.compareTo(str2);
+                }
+//                return 0;
+                throw new RuntimeException("输入的数据类型不一致");
+            }
+        });
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
